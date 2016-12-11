29a309a5e3dc948e7af0bf626a92b3c4f477b56f
Antonio <amegia@gmail.com>
2016-12-12 00:39:53 +0100

    Cabecera de la liga

--- app/src/main/java/com/isanalva/footballteams/ui/teams/TeamsActivity2.java
+++ app/src/main/java/com/isanalva/footballteams/ui/teams/TeamsActivity2.java
@@ -2,22 +2,28 @@ package com.isanalva.footballteams.ui.teams;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.isanalva.footballteams.R;
 import com.isanalva.footballteams.domain.FootballTeam;
 import com.isanalva.footballteams.domain.LeagueTeam;
 
 import java.util.List;
 
-public class TeamsActivity2 extends Activity implements TeamsView2, AdapterView.OnItemClickListener {
+public class TeamsActivity2 extends AppCompatActivity implements TeamsView2, AdapterView.OnItemClickListener {
 
     private ProgressBar progressBar;
     private TeamsPresenter2 presenter;
@@ -30,12 +36,55 @@ public class TeamsActivity2 extends Activity implements TeamsView2, AdapterView.
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teams);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        initCollapsingToolbar();
+
         recycler = (RecyclerView) findViewById(R.id.teams_recycler_view);
         recycler.setHasFixedSize(true);
+
         lManager = new LinearLayoutManager(this);
         recycler.setLayoutManager(lManager);
         progressBar = (ProgressBar) findViewById(R.id.progress);
+        try {
+            Glide.with(this).load(R.drawable.logolaliga).into((ImageView) findViewById(R.id.backdrop));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         presenter = new TeamsPresenterImpl2(this);
+    }
+
+    /**
+     * Initializing collapsing toolbar
+     * Will show and hide the toolbar title on scroll
+     */
+    private void initCollapsingToolbar() {
+        final CollapsingToolbarLayout collapsingToolbar =
+                (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+        collapsingToolbar.setTitle(" ");
+        AppBarLayout appBarLayout = (AppBarLayout) findViewById(R.id.appbar);
+        appBarLayout.setExpanded(true);
+
+        // hiding & showing the title when toolbar expanded & collapsed
+        appBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+            boolean isShow = false;
+            int scrollRange = -1;
+
+            @Override
+            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+                if (scrollRange == -1) {
+                    scrollRange = appBarLayout.getTotalScrollRange();
+                }
+                if (scrollRange + verticalOffset == 0) {
+                    collapsingToolbar.setTitle(getString(R.string.app_name));
+                    isShow = true;
+                } else if (isShow) {
+                    collapsingToolbar.setTitle(" ");
+                    isShow = false;
+                }
+            }
+        });
     }
 
     @Override
